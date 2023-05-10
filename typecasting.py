5dddb15fd55e804bfac37d8c1335d0abc8f49a53
itsabhishekkr <132292924+itsabhishekkr@users.noreply.github.com>
2023-05-10 19:07:40 +0530

    python

---
+++ typecasting.py
@@ -0,0 +1,13 @@
+#typecasting covert one datatype to another datatype
+#variety of function like ex-int(),float(),str(),ord(),hex(),oct(),tuple(),set(),list(),dict()
+                 #types 0f typecasting
+#1.explicit typecasting = convert by developer by manually
+#2.implicit typecasting =
+
+a='1' #or a=1
+b='5'#0r b=5
+print(a+b)
+print(int(a)+int(b))#string covert into integer by explicit typecasting
+print(int(a)*int(b))
+a=8.5;b=2
+print(a+b)
